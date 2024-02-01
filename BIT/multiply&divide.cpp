1ab014dfa18e8c9139b95083d402000411474546
Shaon Roy <113965094+shaon508@users.noreply.github.com>
2024-02-01 21:31:06 +0600

    Create multiply&divide.cpp

---
+++ BIT/multiply&divide.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int MAX_N = 1009;
+int main()
+{
+    int n;
+    cin>>n;
+    //divide by 2
+    cout<<(n>>1)<<endl;
+    //multiply by 2
+    cout<<(n<<1)<<endl;
+    return 0;
+}
