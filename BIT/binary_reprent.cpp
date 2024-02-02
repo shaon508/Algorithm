7592ad0ffcf7d3b4014f136e3cc556edf4c36d3c
Shaon Roy <113965094+shaon508@users.noreply.github.com>
2024-02-02 15:16:00 +0600

    Update binary_reprent.cpp

--- BIT/binary_reprent.cpp
+++ BIT/binary_reprent.cpp
@@ -1,9 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int MAX_N = 1009;
-void print(int num)
+void decTObin(int num)
 {
-    for(int i=10; i>=0; i--)
+    // num is 64 to bit integer than start loop from 63
+    for(int i=31;i>=0;i--)
     {
         cout<<((num>>i)&1);
     }
@@ -11,9 +11,8 @@ void print(int num)
 }
 int main()
 {
-    for(int i=0; i<=8; i++)
-    {
-        print(i);
-    }
+    int num;
+    cin>>num;
+    decTObin(num);
     return 0;
 }
